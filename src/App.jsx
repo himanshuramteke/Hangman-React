a0180a7d6dfa7e4e2773ecaac60bbf6a73be4808
Himanshu Ramteke <himanshuramteke007@gmail.com>
2024-09-12 22:59:13 +0530

    modified main/App files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import StartGame from './pages/StartGame'
+import PlayGame from './pages/PlayGame'
 
 function App() {
  
 
   return (
-   <h1 className='text-3xl text-white bg-black'>Hello world</h1>
+    <Routes>
+      <Route path='/start' element={<StartGame />} />
+      <Route path='/start' element={<PlayGame />} />
+      <Route path='/start' element={<div>Home</div>} />
+    </Routes>
   )
 }
 
