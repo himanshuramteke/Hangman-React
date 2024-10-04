adf0021c483b31f811abd78db0c7fd6f72d78b8e
Himanshu Ramteke <himanshuramteke007@gmail.com>
2024-10-04 20:27:43 +0530

    updated pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom";
 import Button from "../components/Button/Button";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import useWordStore from "../stores/WordStore";
 
 function Home() {
-    const [word, setWord] = useState('');
+    
+    const { setWordList, setWord } = useWordStore();
 
     async function fetchWords() {
-        const response = await fetch();
+        const response = await fetch('http://localhost:3000/words');
         const data = await response.json();
         console.log(data);
+
+        setWordList([...data])
 
         const randomIndex = Math.floor(Math.random() * data.length);
         console.log(data[randomIndex]);
@@ -22,7 +26,8 @@ function Home() {
 
     return (
         <>
-          <Link to='/play' state={{ wordSelected: word }}>  
+          <Link to='/play'>  
+            <Button text="Single player"/>
           </Link>
           <br />
           <Link to="/start">
--- src/pages/PlayGame.jsx
+++ src/pages/PlayGame.jsx
@@ -1,16 +1,21 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Maskedtext from "../components/MaskedText/MaskedText";
+import LetterButtons from "../components/LetterButtons/LetterButtons";
 import { useState } from "react";
+import Hangman from "../components/Hangman/Hangman";
+import useWordStore from "../stores/WordStore";
 
 function PlayGame() {
 
-    const { state } = useLocation();
+    //const { state } = useLocation();
 
+    const { wordList, word } = useWordStore();
     const [guessedLetters, setGuessedLetters] = useState([]);
 
     const [step, setStep] = useState(0);
 
     function handleLetterClick(letter) {
-        if(state.wordSelected.toUpperCase().inlcudes(letter)) {
+        if(word?.toUpperCase().includes(letter)) {
             console.log('Correct');
         } else {
             console.log('Wrong');
@@ -23,6 +28,24 @@ function PlayGame() {
     return (
         <>
           <h1>Play Game</h1>
+
+          {wordList.map((word) => {
+            return <li key={word.id}>{word.wordValue}</li>
+          })}
+
+          {word && (
+            <>
+              <Maskedtext text={word} guessedLetters={guessedLetters} />
+              <div>
+                <LetterButtons text={word} guessedLetters={guessedLetters} onLetterClick={handleLetterClick} />
+              </div>
+              <div>
+                <Hangman step={step} />
+              </div>
+            </>
+          )}
+
+          <Link to='/'>Home</Link>
           <Link to='/start' className="text-blue-400">Start Game Link</Link>
         </>
     );
